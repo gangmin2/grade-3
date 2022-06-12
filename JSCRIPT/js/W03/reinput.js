47b37bf9e7d7f6e1664a1a0e7ddf7e64a1c4fe2e
min <96777845+gangmin2@users.noreply.github.com>
2022-06-12 19:06:35 +0900

    폴더 구조 변경

--- JSCRIPT/js/W03/reinput.js
+++
@@ -1,9 +0,0 @@
-const repl = require('repl')
-
-repl.start({
-    prompt: "입력 때 앞에 출력할 문자열",
-    eval: (command, context, filename, callback) => {
-        console.log(command)
-        callback()
-    }
-})
