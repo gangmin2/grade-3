623806bb4aa9fd1499a38e7d38788832e2d7be5c
min <96777845+gangmin2@users.noreply.github.com>
2022-06-13 09:33:27 +0900

    DOM을 이용하여 HTML 객체 저장

--- Software-Architecture/swProject/app/src/public/js/home/login.js
+++ Software-Architecture/swProject/app/src/public/js/home/login.js
@@ -1,1 +1,13 @@
-console.log("hello");
+const login = () => {
+    const req = {
+        id: id.value,
+        pw: pw.value,
+    };
+    console.log(req);
+};
+
+const id = document.querySelector("#id");
+const pw = document.querySelector("#pw");
+const loginButton = document.querySelector("#login-button");
+
+loginButton.addEventListener("click", login);
