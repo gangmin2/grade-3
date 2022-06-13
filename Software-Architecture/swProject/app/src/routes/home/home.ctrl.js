dfbff56abefa8299be518313f401160963dd48cd
min <96777845+gangmin2@users.noreply.github.com>
2022-06-13 22:54:13 +0900

    html-css 연결

--- Software-Architecture/swProject/app/src/routes/home/home.ctrl.js
+++ Software-Architecture/swProject/app/src/routes/home/home.ctrl.js
@@ -1,4 +1,4 @@
-const UserStorage = require("../../models/UserStorage")
+const User = require("../../models/User");
 
 const output = {
     home: (req, res) => {
@@ -14,23 +14,9 @@ const output = {
 
 const process = {
     login: (req, res) => {
-        const id = req.body.id;
-        const pw = req.body.pw;
-
-        const users = UserStorage.getUsers("id", "pw");
-
-        const response = {};
-        if (users.id.includes(id)) {
-            const idx = users.id.indexOf(id);
-            if (users.pw[idx] == pw) {
-                response.success = true;
-                return res.json(response)
-            }
-        }
-
-        response.success = false;
-        response.msg = "로그인에 실패했습니다.";
-        return res.json(response)
+        const user = new User(req.body);
+        const response = user.login();
+        return res.json(response);
     },
 }
 
